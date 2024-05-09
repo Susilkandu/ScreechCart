22f2f295a363a496790029d0b32f2a13b4b49966
Sushil Kandu <youfriendy324@gmail.com>
2024-05-09 16:02:48 +0530

    LoginSellerAccount ui created

--- frontEnd/src/components/seller/LoginSellerAccount.jsx
+++ frontEnd/src/components/seller/LoginSellerAccount.jsx
@@ -1,14 +1,61 @@
-import React, { useCallback, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginSellerAccount } from "../../assets/api/seller/authApi";
 export default function LoginSellerAccount() {
-    const navigate=useNavigate();
-    useEffect(() => {
-        if(localStorage.getItem('sToken')){
-            navigate('/sellerDashboard');
-            }
-    }, [])
-    
+  const navigate = useNavigate();
+  const [mbl,setMbl]=useState('');
+  const [psd,setPsd]=useState('');
+  const loginHandler = async () => {
+    const response = await loginSellerAccount(mbl, psd);
+    if (response.ackbool == 1) {
+      navigate("/sellerDashboard");
+    }
+  };
+  const loginBtnRef = useRef(null);
+  useEffect(() => {
+    if (localStorage.getItem("sToken")) {
+      navigate("/sellerDashboard");
+    }
+    const loginBtn = loginBtnRef.current;
+    return () => {
+      if (loginBtn) {
+        loginBtn.removeEventListener("click", loginHandler);
+      }
+    };
+  }, []);
   return (
-    <div>LoginSellerAccount</div>
-  )
+    <div className="login-slr-form">
+      <span className="material-symbols-outlined user-logo">person</span>
+      <div className="userName">
+        <span
+          className="material-symbols-outlined usernameLogo"
+          style={{ fontSize: "2rem" }}
+        >
+          person
+        </span>
+        <input type="text" onChange={(e)=>{setMbl(e.target.value)}} placeholder="username" min={10} max={10} />
+      </div>
+      <div className="password">
+        <span
+          className="material-symbols-outlined"
+          style={{ fontSize: "2rem" }}
+        >
+          lock
+        </span>
+        <input type="password" onChange={(e)=>{setPsd(e.target.value)}} placeholder="password" />
+      </div>
+      <section className="login-btn-holder">
+        <span
+          className="material-symbols-outlined"
+          style={{ fontSize: "2rem" }}
+        >
+          {" "}
+          login
+        </span>
+        <button className="login-btn" ref={loginBtnRef} onClick={loginHandler}>
+          Login
+        </button>
+      </section>
+    </div>
+  );
 }
